8ca4beed6b1948393b919b7131fab74ee2ea1199
Varshini895 <varshinireddynallamilli@gmail.com>
2024-07-05 16:07:00 +0530

    Update Soundex.Tests.cpp_Varshini

--- Soundex.Tests.cpp
+++ Soundex.Tests.cpp
@@ -6,4 +6,12 @@ TEST(SoudexTestsuite, ReplacesConsonantsWithAppropriateDigits) {
   char soundex[5];
   generateSoundex("AX", soundex);
  // ASSERT_EQ(soundex,"A200");
+ generateSoundex("AVPHF", soundex);
+ // ASSERT_EQ(soundex,"A100");
+ generateSoundex("EVAPH", soundex);
+ // ASSERT_EQ(soundex,"E110");
+ generateSoundex("EXCELLENT", soundex);
+ // ASSERT_EQ(soundex,"E245");
+ generateSoundex("ROBERT", soundex);
+ // ASSERT_EQ(soundex,"R163");
 }
