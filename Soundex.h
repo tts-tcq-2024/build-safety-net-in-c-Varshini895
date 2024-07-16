301f3e83f28b43f92fd3618b76c1c87e7f9c98be
Varshini895 <varshinireddynallamilli@gmail.com>
2024-07-16 23:46:17 +0530

    Update Soundex.h

--- Soundex.h
+++ Soundex.h
@@ -6,44 +6,47 @@
 #include <string.h>
 
 char getSoundexCode(char c) {
-    c = toupper(c);
-    switch (c) {
-        case 'B': case 'F': case 'P': case 'V': return '1';
-        case 'C': case 'G': case 'J': case 'K': case 'Q': case 'S': case 'X': case 'Z': return '2';
-        case 'D': case 'T': return '3';
-        case 'L': return '4';
-        case 'M': case 'N': return '5';
-        case 'R': return '6';
-        default: return '0'; // For A, E, I, O, U, H, W, Y
-    }
-}
 
+    static const char soundexTable[26] = {
+
+        '0', '1', '2', '3', '0', '1', '2', '0', '0', '2', '2', '4', '5', '5', '0', '1', '2', '6', '2', '3', '0', '1', '0', '2', '0', '2'
+
+    };
+ 
+    c = toupper(c);
+
+    if (c >= 'A' && c <= 'Z') {
+
+        return soundexTable[c - 'A'];
+
+    }
+
+    return '0'; // For non-alphabet characters
+
+}
+ 
+ 
 void generateSoundex(const char *name, char *soundex) {
-    int len = strlen(name);
-    int temp;
-    if (len == 0) {
+    if (name[0] == '\0') {
         strcpy(soundex, "0000");
         return;
     }
+
     soundex[0] = toupper(name[0]);
     int sIndex = 1;
-
     char lastDigit = getSoundexCode(name[0]);
-    for (int i = 1; i < len && sIndex < 4; i++) {
+
+    for (int i = 1; name[i] != '\0' && sIndex < 4; i++) {
         char code = getSoundexCode(name[i]);
-        if (!(strchr("aeiou", name[i-1]))){
-            temp = 0;
-        }
-        if (strchr("aeiou", name[i])){
-            temp = 1;
-        }
-        if (strchr("hw", name[i])){
+        char upperChar = toupper(name[i]);
+
+        if (code == '0' || code == lastDigit || strchr("AEIOUYHW", upperChar)) {
+            if (strchr("AEIOU", upperChar)) lastDigit = '0';
             continue;
         }
-        if ((code != '0' && code != soundex[sIndex - 1]) || (code != '0' && lastDigit == code && temp)) {
-            soundex[sIndex++] = code;
-            lastDigit = code;
-        }
+
+        soundex[sIndex++] = code;
+        lastDigit = code;
     }
 
     while (sIndex < 4) {
